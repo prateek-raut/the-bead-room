93f9173504739a809222ca24e92287fdb13a99bb
Prateek Raut <prateek.raut26@gmail.com>
2026-08-24 16:47:54 +0530

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -3,8 +3,8 @@ title The Bead Room by Pallas - E-Commerce Server
 echo ========================================================
 echo   🌸 Starting The Bead Room by Pallas Server 🎨
 echo ========================================================
-echo   Web Storefront : http://localhost:3000/
-echo   Admin Portal   : http://localhost:3000/admin.html
+echo   Web Storefront : https://github.com/prateek-raut/the-bead-room
+echo   Admin Portal   : https://github.com/prateek-raut/the-bead-room/admin.html
 echo   Store Email    : sarakamdar26@gmail.com
 echo ========================================================
 echo.
